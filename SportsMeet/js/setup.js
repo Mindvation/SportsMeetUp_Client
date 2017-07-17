a267a3eef53ffd6d20ac13407905726f9b5540d1
guobg <guobg1987@163.com>
2017-07-17 14:59:18 +0800

    upload project

---
+++ SportsMeet/js/setup.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class setup extends Component {
+    render() {
+        return (
+            <View style={[styles.container]}>
+                <Text>Logon Page</Text>
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#77d4ff',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
